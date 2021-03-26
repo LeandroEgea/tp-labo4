babb16411b88e3f59367dc562bfad2d1d51e3b8a
LeandroEgea <leandronicolas.egea@mercadolibre.com>
2021-03-25 21:34:05 -0300

    install firebase and update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBOJn7PTiHj51DxqJCPw-XUV64Q_TKY7so',
+    authDomain: 'tp-labo4-1d7fb.firebaseapp.com',
+    projectId: 'tp-labo4-1d7fb',
+    storageBucket: 'tp-labo4-1d7fb.appspot.com',
+    messagingSenderId: '426688509678',
+    appId: '1:426688509678:web:d0f79c4dfc97e23b69a5ff',
+  },
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
