a416c1d6840ef8e59a93fbad072a168769446100
LeandroEgea <leandronicolas.egea@mercadolibre.com>
2021-04-04 20:04:11 -0300

    add functional registro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: 'quien-soy', component: QuienSoyComponent },
   { path: '**', component: HomeComponent },
 ];
