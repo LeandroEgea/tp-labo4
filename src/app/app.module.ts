3f846524b99f2e6cf45665e57e146cc5a86e859a
LeandroEgea <leandronicolas.egea@mercadolibre.com>
2021-04-04 17:36:28 -0300

    add firebase in app.module.ts and create class user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -12,13 +16,16 @@ import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    QuienSoyComponent
+    QuienSoyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/classes/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  uid: string;
+  email: string;
+  password: string;
+  displayName: string;
+  photoURL: string;
+  //emailVerified: boolean;
+}
