3d525225326663ff6fe52fb205b00aa71950d3f5
LeandroEgea <leandronicolas.egea@mercadolibre.com>
2021-04-04 21:57:06 -0300

    environment for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBOJn7PTiHj51DxqJCPw-XUV64Q_TKY7so',
+    authDomain: 'tp-labo4-1d7fb.firebaseapp.com',
+    projectId: 'tp-labo4-1d7fb',
+    storageBucket: 'tp-labo4-1d7fb.appspot.com',
+    messagingSenderId: '426688509678',
+    appId: '1:426688509678:web:d0f79c4dfc97e23b69a5ff',
+  },
 };
